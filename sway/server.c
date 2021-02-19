97d3d6424dbe69ac0f5d39110961d14b8824ae14
Daniel De Graaf <code@danieldg.net>
2021-02-18 23:03:51 -0500

    Label-based security on privileged globals
    This adds a command (security_label) which is used to allow or deny access to
    privileged interfaces on a client-by-client basis. If no security configuration
    it present, all privileged operations are allowed to all clients.
    
     - "security_label deny default *" will deny clients access to all privileged
       operations
     - "security_label set default layer_shell" will overwrite the access list for
       the label "default" and only allow access to the layer_shell interface
     - "security_label permit recorder screencopy_manager" allows connections that
       have the label "recorder" access to screencopy_manager in addition to the
       current permissions of the "recorder" label
    
    If a client does not have a label or if the label's permissions were not
    defined using security_label, the permissions for the "default" label are used;
    if no definition for "default" is present, all interfaces are allowed.
    
    Using permit or deny on a new label does not copy the default.
    
    The security configuration state is reset on a config reload (similar to the
    assign and for_window lists).  Currently, the security policy is only enforced
    during the binding or enumeration of global resources; existing handles to
    privileged interfaces are not invalided by a change in policy, and existing
    clients are not informed of the presence of newly available interfaces.

--- sway/server.c
+++ sway/server.c
@@ -38,6 +38,7 @@
 #include "sway/input/input-manager.h"
 #include "sway/output.h"
 #include "sway/server.h"
+#include "sway/security.h"
 #include "sway/tree/root.h"
 #if HAVE_XWAYLAND
 #include "sway/xwayland.h"
@@ -72,7 +73,7 @@ bool server_init(struct sway_server *server) {
 	server->data_device_manager =
 		wlr_data_device_manager_create(server->wl_display);
 
-	wlr_gamma_control_manager_v1_create(server->wl_display);
+	server->gamma_control_manager = wlr_gamma_control_manager_v1_create(server->wl_display);
 
 	server->new_output.notify = handle_new_output;
 	wl_signal_add(&server->backend->events.new_output, &server->new_output);
@@ -148,9 +149,9 @@ bool server_init(struct sway_server *server) {
 	server->foreign_toplevel_manager =
 		wlr_foreign_toplevel_manager_v1_create(server->wl_display);
 
-	wlr_export_dmabuf_manager_v1_create(server->wl_display);
-	wlr_screencopy_manager_v1_create(server->wl_display);
-	wlr_data_control_manager_v1_create(server->wl_display);
+	server->dmabuf_manager = wlr_export_dmabuf_manager_v1_create(server->wl_display);
+	server->screencopy_manager = wlr_screencopy_manager_v1_create(server->wl_display);
+	server->data_control_manager = wlr_data_control_manager_v1_create(server->wl_display);
 	wlr_primary_selection_v1_device_manager_create(server->wl_display);
 	wlr_viewporter_create(server->wl_display);
 
@@ -158,6 +159,8 @@ bool server_init(struct sway_server *server) {
 		wlr_xdg_foreign_registry_create(server->wl_display);
 	wlr_xdg_foreign_v1_create(server->wl_display, foreign_registry);
 	wlr_xdg_foreign_v2_create(server->wl_display, foreign_registry);
+
+	wl_display_set_global_filter(server->wl_display, security_global_filter, server);
 
 	// Avoid using "wayland-0" as display socket
 	char name_candidate[16];
